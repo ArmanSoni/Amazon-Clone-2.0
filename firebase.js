09892ad39fbe2799283ec3e423b9944fbb932a76
ArmanSoni <50861055+ArmanSoni@users.noreply.github.com>
2021-09-03 22:12:26 +0400

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDGdFy3zHJhrvGAMQ_JKCPgnX6V_Y9Cg-k",
+    authDomain: "clone-2-4cb9a.firebaseapp.com",
+    projectId: "clone-2-4cb9a",
+    storageBucket: "clone-2-4cb9a.appspot.com",
+    messagingSenderId: "238873616700",
+    appId: "1:238873616700:web:62dfd71bf704b7b6c2fe39"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var db = firebase.firestore();
